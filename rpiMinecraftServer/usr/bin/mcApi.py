b37afdb0691ce9ad7937c7a334031e58e418b902
afercin <afercin@gmail.com>
2022-03-27 15:55:45 +0200

    added api rest

---
+++ rpiMinecraftServer/usr/bin/mcApi.py
@@ -0,0 +1,73 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+from mcServer import MinecraftServer
+import os
+
+app = Flask(__name__)
+api = Api(app)
+parser = reqparse.RequestParser()
+
+confFile = "/etc/productConf/mc.conf"
+
+if "dev" in os.path.abspath(os.getcwd()):
+    confFile = "/home/afercin/dev/RPI-Minecraft-Server/rpiMinecraftServer" + confFile
+
+mcserver = MinecraftServer(confFile)
+
+
+class Start(Resource):
+    def get(self):
+        if mcserver.isRunning:
+            return "Minecraft server is already running", 403
+
+        mcserver.start()
+        return "Starting minecraft server", 202
+
+
+class Stop(Resource):
+    def get(self):
+        if not mcserver.isRunning:
+            return "Minecraft server is not running", 403
+
+        if not mcserver.isClossing:
+            return "Minecraft server is being clossing", 403
+
+        mcserver.stop()
+        return "Stopping minecraft server", 202
+
+
+class GetStatus(Resource):
+    def get(self):
+        if mcserver.isClossing:
+            return "Clossing", 200
+        if mcserver.isReady:
+            return "Ready", 200
+        if mcserver.isRunning:
+            return "Starting", 200
+
+        return "Closed", 200
+
+
+class GetOutput(Resource):
+    def get(self):
+        return mcserver.getOutput()
+
+class SendCommand(Resource):
+    def post(self, command):
+        if not mcserver.isRunning:
+            return "Minecraft server is not running", 503
+        if not mcserver.isReady:
+            return "Minecraft server is not ready", 503
+
+        mcserver.sendCommand(command)
+        return "Command send sucessfully", 200
+
+
+api.add_resource(Start, "/start/")
+api.add_resource(Stop, "/stop/")
+api.add_resource(GetStatus, "/status/")
+api.add_resource(GetOutput, "/output/")
+api.add_resource(SendCommand, "/command/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- rpiMinecraftServer/usr/bin/mcServer.py
+++ rpiMinecraftServer/usr/bin/mcServer.py
@@ -18,11 +18,18 @@ class MinecraftServer:
         self.maxRam = config["SERVER"]["MaxRAM"]
         self.minRam = config["SERVER"]["MinRAM"]
 
+        self.reset()
+        self.outputHook = threading.Thread(target=self.captureOutput)
+
+    def reset(self):
         self.isRunning = False
-        self.outputHook = threading.Thread(target=self.getOutput)
+        self.isClossing = False
+        self.isReady = False
+        self.output = ""
 
     def start(self):
         if not self.isRunning:
+            self.reset()
             forgeFile = "forge-{}.jar".format(self.forgeVersion)
             self.log.info("Starting minecraft server...")
 
@@ -42,18 +49,29 @@ class MinecraftServer:
             self.outputHook.start()
 
     def stop(self):
-        if self.isRunning:
-            self.log.info("Stopping server...")
+        def stopping():
             self.sendCommand("stop")
             self.minecraftProcess.wait()
             self.log.ok("Server stopped!")
-            self.isRunning = False
+            self.reset()
+
+        if self.isRunning:
+            self.log.info("Stopping server...")
+            self.isClossing = True
+            time = 5
+            threading.Thread(target=stopping).start()
 
     def sendCommand(self, command):
         if self.isRunning:
             self.minecraftProcess.communicate(input="{}\n".format(command))
 
-    def getOutput(self):
+    def captureOutput(self):
+        def processOutput(output):
+            self.output += "{}\n".format(output)
+            print(output, flush=True)
+            if "[Server thread/INFO] [minecraft/DedicatedServer]: Done" in output:
+                self.isReady = True
+
         self.log.info("Hook initialized")
         while self.isRunning:
             realtime_output = self.minecraftProcess.stdout.readline()
@@ -62,12 +80,10 @@ class MinecraftServer:
                 break
 
             if realtime_output:
-                self.processOutput(realtime_output.strip())
-    
-    def processOutput(self, output):
-        print(output, flush=True)
-        if "[Server thread/INFO] [minecraft/DedicatedServer]: Done" in output:            
-            self.log.ok("¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡ Mundo listo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                processOutput(realtime_output.strip())
+
+    def getOutput(self):
+        return self.output
 
 
 if __name__ == "__main__":
